030890ea5ad1119be6cfc26ca53ebe4a5e9793f3
zmahmood-github <56387744+zmahmood-github@users.noreply.github.com>
2020-01-24 21:44:15 +0500

    src/lib.rs created n pushed

---
+++ lib/src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub mod military{
+    pub mod weapon{
+       pub  fn machinegun(){
+             println!("machinegun is an automatic long range weapon.");
+         }
+     }
+ }
